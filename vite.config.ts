dac9ac6cbd5201742f6ac74c1161c853daf70808
antenar <antennarr@gmail.com>
2025-08-22 14:53:28 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/antenar.github.io/",   // <-- replace with your repo name if different
+  base: "/",   // <-- replace with your repo name if different
   server: {
     port: 8080,
   },
