a78dafdfe2d7a6af76d822b5ec2b3b6b44de9f42
antenar <antennarr@gmail.com>
2025-08-22 15:21:16 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,7 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
-import { componentTagger } from "lovable-tagger";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  base: "/",   // <-- replace with your repo name if different
-  server: {
-    port: 8080,
-  },
-  plugins: [
-    react(),
-    mode === "development" && componentTagger(),
-  ].filter(Boolean),
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-}));
+export default defineConfig({
+  plugins: [react()],
+  base: "/", // 👈 must be root for username.github.io repos
+});
